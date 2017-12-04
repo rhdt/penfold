ab4f097e5f549f83e445d95d901412e90e603c44
sgk <syamgk01@gmail.com>
2017-12-04 13:15:08 +0530

    add mongodb template

---
+++ mongodb/cico_build_deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Show command before executing
+set -x
+
+# Exit on error
+set -e
+
+export BUILD_TIMESTAMP=`date -u +%Y-%m-%dT%H:%M:%S`+00:00
+
+# Check the existence of image on registry.centos.org
+IMAGE="mongodb-32-centos7"
+REGISTRY="https://registry.centos.org"
+REPOSITORY="sclo"
+TEMPLATE="openshift/mongodb.app.yaml"
+
+#Find tag used by deployment template
+echo -e "Scanning OpenShift Deployment Template for Image tag"
+TAG=$(cat $TEMPLATE | grep -A 1 "name: IMAGE_TAG_VERSION" | grep "value:" |  awk '{split($0,array,":")} END{print array[2]}')
+
+#Check if image is in the registry
+echo -e "Checking if image exists on the registry"
+TAGLIST=$(curl -X GET $REGISTRY/v2/$REPOSITORY/$IMAGE/tags/list)
+echo $TAGLIST | grep -w $TAG
+
+if [ $? -eq 0 ]; then
+  echo 'CICO: Image existence check successful. Ready to deploy updated app'
+  exit 0
+else
+  echo 'CICO: Image existence check failed. Exiting'
+  exit 2
+fi
